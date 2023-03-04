4fc73cc08da9c6054bda2f0a424e3a11fd48367d
Alan.Chu <achu61@seas.upenn.edu>
2023-03-04 14:03:02 -0800

    initial setup + DB connected

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // dependencies
 const express = require('express');
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 // initialize app
 const app = express();
@@ -7,12 +10,20 @@ const app = express();
 // configure settings
 require('dotenv').config();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL
 
 // connect database
-
+mongoose.set('strictQuery', false)
+mongoose.connect(DATABASE_URL);
+mongoose.connection
+  .on("open", () => console.log("You are connected to MongoDB"))
+  .on("close", () => console.log("You are disconnected from MongoDB"))
+  .on("error", (error) => console.log(`MongoDB Error: ${error.message}`));
 
 // mount middleware
-
+app.use(cors()); 
+app.use(morgan("dev")); 
+app.use(express.json()); 
 
 // test route
 app.get('/', (req, res) => {

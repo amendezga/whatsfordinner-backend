f5d796f24aab983d3bc2712d280be95eb624adfd
Sophie Delice <delicesophie824@gmail.com>
2023-03-15 22:39:56 -0400

    adds helmetJS

--- server.js
+++ server.js
@@ -27,6 +27,38 @@ app.use(cors());
 app.use(morgan("dev")); 
 app.use(express.json()); 
 
+
+// adding helmetJS 
+const helmet = require("helmet");
+app.use(helmet.hidePoweredBy());
+
+//Mitiagte the risk of clickjacking. 
+app.use(helmet.frameguard({action: "deny"}));
+
+// Mitigate the Risk of Cross Site Scripting (XSS) Attacks with helmet.xssFilter()
+app.use(helmet.xssFilter());
+
+//This middleware sets the X-Content-Type-Options header to nosniff.
+app.use(helmet.noSniff());
+
+//This middleware sets the X-Download-Options header to noopen.
+app.use(helmet.ieNoOpen());
+
+// Ask Browsers to Access Your Site via HTTPS Only
+const ninetyDaysInSeconds = 90*24*60*60
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force:true}));
+
+// DNS prefetching
+app.use(helmet.dnsPrefetchControl());
+
+// Disable Client-Side Caching
+app.use(helmet.noCache());
+
+
+// Content Security Policy
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ));
+
+
 app.use('/refrigerator',ingredientRouters)
 // test route
 app.get('/', (req, res) => {
